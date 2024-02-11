34778342f3fd92f9b9db86a6f9a7a4d6a202d8e7
최도은 <choidoeun0102@gmail.com>
2024-02-11 23:31:18 +0900

    예약하기 페이지

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-// 한 줄 짜리 주석
 
 /**
  * 메인 페이지 슬라이드 (자동 슬라이드, 일시 정지, 재개 기능 구현)
@@ -22,3 +21,19 @@ function playCarousel() {
     const myCarouselElement = document.querySelector('#carouselExampleIndicators')
     $(myCarouselElement).carousel('cycle');
 }
+
+//Reservation
+function reservation() {
+    //JSON 파일에서 데이터 불러오기
+    async function fetchReservation() {
+        const getReservationJSON = await fetch("../api/reservation.json");
+        const reservationJSON = await getReservationJSON.json();
+        console.log(reservationJSON["reservition"]);
+
+        return reservationJSON["reservition"];
+    }
+
+    //test code
+    // const reservationData = fetchReservation();
+    // console.log(reservationData);  
+} 
