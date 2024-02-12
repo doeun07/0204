6eb422dbbfff62dea4684c7e63a4b2025a7cee1b
최도은 <choidoeun0102@gmail.com>
2024-02-12 16:35:47 +0900

    예약하기_주말,평일 구분

--- js/script.js
+++ js/script.js
@@ -32,8 +32,43 @@ function reservation() {
 
         return reservationJSON["reservition"];
     }
-
     //test code
     // const reservationData = fetchReservation();
     // console.log(reservationData);  
+
+
+    const resDateTableElem = document.querySelector("#resDateTable");
+    for(let i = 0; i < 14; i++) {
+        // 날짜&요일 가져오기
+        const date = new Date();
+        date.setDate(date.getDate() + i);
+        const year = date.getFullYear();
+        const month = ("0" + (date.getMonth() + 1)).slice(-2);
+        const day = ("0" + (date.getDate())).slice(-2);
+        const week = date.getDay();
+        // const weekToString = ((week == 0) || (week == 6)) ? "주말" : "주중";
+        //(조건식 ? 참일 때의 값 : 거짓일 때의 값;)
+        // console.log(`${year}.${month}.${day} (${week})`);
+
+        // 토요일, 일요일 구분 출력
+        if(week == 0) {
+            resDateTableElem.innerHTML += `<th class="sun">${year}.${month}.${day}</th>`;
+        } else if(week == 6) {
+            resDateTableElem.innerHTML += `<th class="sat">${year}.${month}.${day}</th>`;
+        } else {
+            resDateTableElem.innerHTML += `<th>${year}.${month}.${day}</th>`;
+        }
+        
+        //날짜 Table로 출력
+        // resDateTableElem.innerHTML += `<th>${year}.${month}.${day}</th>`;
+    }
+
+    async function printReservation() {
+        const reservation = await fetchReservation();
+        // console.log(reservation[0]["D+0"][0]["status"]);
+        for(let i = 0; i < 16; i++) {
+            document.querySelector(`#row${i}`).innerHTML += `<td>${reservation[0]["D+0"][i]["status"]}</td>`
+        }
+    }
+    printReservation();
 } 
