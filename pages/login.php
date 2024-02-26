3599bd3f832e7dbdb65d60607cb88f0f6b423d14
최도은 <choidoeun0102@gmail.com>
2024-02-26 19:06:12 +0900

    로그인 페이지 만들기

---
+++ pages/login.php
@@ -0,0 +1,15 @@
+<form action="">
+    <div class="mb-3 row">
+    </div>
+    <div class="mb-3 row">
+        <label class="col-sm-2 col-form-label">이름</label>
+        <div class="col-sm-10">
+            <input class="form-control">
+        </div>
+        <label class="col-sm-2 col-form-label">전화번호</label>
+        <div class="col-sm-10">
+            <input class="form-control">
+        </div>
+        <button class="login_button">로그인</button>
+    </div>
+</form>
