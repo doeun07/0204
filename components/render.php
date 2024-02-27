fff6ba3d0f6610d9c1d5b0052f092caf8b3552ec
최도은 <choidoeun0102@gmail.com>
2024-02-27 14:51:55 +0900

    api 구분을 위해 페이지 나눔

---
+++ components/render.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ko">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Skills Camping</title>
+    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="./bootstrap/dist/css/bootstrap.css">
+    <script src="./js/script.js"></script>
+</head>
+
+<body>
+    <?php
+    $includeheaderAndFooter ? include("./components/header.php") : null;
+    include($page);
+    $includeheaderAndFooter ? include("./components/footer.php") : null;
+    ?>
+</body>
+<script src="./jquery/jquery-3.6.0.js"></script>
+<script src="./bootstrap/dist/js/bootstrap.js"></script>
+
+</html>
--- index.php
+++ index.php
@@ -5,66 +5,60 @@ $request = $_SERVER["REQUEST_URI"];
 $path = explode('?', $request);
 $path[1] = isset($path[1]) ? $path[1] : null;
 $resource = explode("/", $path[0]);
-?>
-<!DOCTYPE html>
-<html lang="ko">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Skills Camping</title>
-    <link rel="stylesheet" href="./style.css">
-    <link rel="stylesheet" href="./bootstrap/dist/css/bootstrap.css">
-    <script src="./js/script.js"></script>
-</head>
+$includeheaderAndFooter = false;
+$page = "";
+switch ($resource[1]) {
+    case '':
+        $page = "./pages/main.php";
+        $includeheaderAndFooter = true;
+        break;
+    case 'contents':
+        $page = "./pages/" . $resource[1] . ".php";
+        $includeheaderAndFooter = true;
+        break;
+    case 'reservation':
+        $page = "./pages/" . $resource[1] . ".php";
+        $includeheaderAndFooter = true;
+        break;
+    case 'mypage':
+        $page = "./pages/" . $resource[1] . ".php";
+        $includeheaderAndFooter = true;
+        break;
+    case 'login':
+        $page = './pages/' . $resource[1] . '.php';
+        $includeheaderAndFooter = true;
+        break;
+    case 'logout':
+        $page = './pages/' . $resource[1] . '.php';
+        $includeheaderAndFooter = false;
+        break;
+    case 'admin':
+        $page = './pages/' . $resource[1] . '.php';
+        $includeheaderAndFooter = true;
+        break;
+    case 'mypage':
+        $page = './pages/' . $resource[1] . '.php';
+        $includeheaderAndFooter = true;
+        break;
+    default:
+        $page = "./pages/main.php";
+        $includeheaderAndFooter = true;
+        break;
+}
 
-<body>
-    <?php
-    $includeheaderAndFooter = false;
-    $page = "";
-    switch ($resource[1]) {
-        case '':
-            $page = "./pages/main.php";
-            $includeheaderAndFooter = true;
-            break;
-        case 'contents':
-            $page = "./pages/" . $resource[1] . ".php";
-            $includeheaderAndFooter = true;
-            break;
+if ($resource[1] == "api") {
+    switch ($resource[2]) {
         case 'reservation':
-            $page = "./pages/" . $resource[1] . ".php";
-            $includeheaderAndFooter = true;
-            break;
-        case 'mypage':
-            $page = "./pages/" . $resource[1] . ".php";
-            $includeheaderAndFooter = true;
-            break;
-        case 'login':
-            $page = './pages/' . $resource[1] . '.php';
-            $includeheaderAndFooter = true;
-            break;
-        case 'logout':
-            $page = './pages/' . $resource[1] . '.php';
-        case 'admin':
-            $page = './pages/' . $resource[1] . '.php';
-            $includeheaderAndFooter = true;
-            break;
-        case 'mypage':
-            $page = './pages/' . $resource[1] . '.php';
-            $includeheaderAndFooter = true;
+            $page = './api/' . $resource[2] . '.php';
+            $includeheaderAndFooter = false;
             break;
         default:
-            $page = "./pages/main.php";
-            $includeheaderAndFooter = true;
+            echo "잘못된 접근입니다.";
             break;
     }
-    $includeheaderAndFooter ? include("./components/header.php") : null;
     include($page);
-    $includeheaderAndFooter ? include("./components/footer.php") : null;
-    ?>
-
-</body>
-<script src="./jquery/jquery-3.6.0.js"></script>
-<script src="./bootstrap/dist/js/bootstrap.js"></script>
-
-</html>
+} else {
+    include("./components/render.php");
+}
+?>
