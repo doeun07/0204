80643283d81e8519cadb5e16fb4d25b77cb638a1
최도은 <choidoeun0102@gmail.com>
2024-02-27 16:36:12 +0900

    데이터 순서 정렬

--- api/reservation.php
+++ api/reservation.php
@@ -1,5 +1,7 @@
 <?php
-    $sql = "SELECT * FROM reservations";
+    $sql = "SELECT * FROM reservations 
+        ORDER BY CAST(SUBSTRING(date, LOCATE('+', date) + 1) AS UNSIGNED) ASC,
+        position ASC";
     $stmt = $pdo->prepare($sql);
     $stmt->execute();
     $reservations = $stmt->fetchAll(PDO::FETCH_ASSOC);
