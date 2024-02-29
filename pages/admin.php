e3320b5072b03e3d4fed87af46174b47502ab6da
최도은 <choidoeun0102@gmail.com>
2024-02-29 14:37:03 +0900

    로그인 검증 및 관리자 검증

---
+++ pages/admin.php
@@ -0,0 +1,13 @@
+<?php
+if (isset($_SESSION["user_idx"])) {
+    if (isset($_SESSION["is_admin"]) == false) {
+        echo "<script>alert('관리자만 이용 가능한 페이지입니다.')
+        location.href = './mypage'
+        </script>";
+    }
+} else {
+        echo "<script>alert('로그인 후 이용 가능합니다.')
+        location.href = './login'
+        </script>";
+}
+?>
